9504ab93a0bb6191455140471e06502a302f9b69
edumahmoud <144780054+edumahmoud@users.noreply.github.com>
2025-09-22 13:56:43 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,31 @@
+/*
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("weget-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
+*/
+
+const CACHE_NAME = "weget-cache-v2";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -9,6 +34,22 @@ self.addEventListener("install", e => {
       ]);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
