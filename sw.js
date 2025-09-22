31043a6a26bf60b09985fe6acfab632f622d6cab
edumahmoud <144780054+edumahmoud@users.noreply.github.com>
2025-09-22 14:58:39 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@ self.addEventListener("fetch", e => {
     })
   );
 });
-*/
+.......
 
 const CACHE_NAME = "weget-cache-v3.0.0";
 
@@ -56,6 +56,53 @@ self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(response => {
       return response || fetch(e.request);
+    })
+  );
+});
+*/
+
+const CACHE_NAME = "weget-cache-" + new Date().getTime();
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./images.png"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return fetch(e.request)
+        .then(fresh => {
+          if (cached) {
+            caches.open(CACHE_NAME).then(cache => cache.put(e.request, fresh.clone()));
+          }
+          return fresh;
+        })
+        .catch(() => cached);
     })
   );
 });
